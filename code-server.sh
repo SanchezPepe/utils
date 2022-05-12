23bdaac7c0a744668296ef7d6a52c5e83199eb11
Pepe Sánchez <SanchezPepe@users.noreply.github.com>
2022-05-11 22:55:27 -0500

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -1,7 +1,44 @@
 #!/bin/sh
 
-# Download
-curl -fsSL https://code-server.dev/install.sh | sh
+# Get the latest package list
+sudo apt update
 
-# Run server
-code-server --auth none --port 8080
+# Do the updates
+sudo apt-get update
+sudo apt --fix-broken install
+
+# Install the Google Cloud SDK
+sudo apt-get update 
+sudo apt-get install -y google-cloud-sdk
+
+# Install Visual Studio Code
+sudo snap install --classic code
+
+# Install Node.js
+sudo apt install -y nodejs
+
+# Install Npm
+sudo apt install -y npm
+
+# Install Vue.js
+# latest stable
+npm install vue
+
+# Clear NPM cache
+npm cache clean -f
+
+# Update NPM
+npm install -g npm@latest
+
+# node version manager
+npm install -g n
+
+# Upgrade Node
+sudo n stable
+
+# Git config
+git config --global user.name "sanchezpepe"
+git config --global user.email sanchezpepe97@gmail.com
+
+# Code Server Install
+curl -fsSL https://code-server.dev/install.sh | sh
