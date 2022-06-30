bccbe4f3828c2103b9c4e37ca9bdba1436f0936b
Pepe Sánchez <SanchezPepe@users.noreply.github.com>
2022-06-29 19:57:30 -0500

    Create create-vm.sh

---
+++ create-vm.sh
@@ -0,0 +1,13 @@
+# Create VM
+gcloud compute instances create dev-vm \
+--project=jjsa-dev \
+--zone=us-central1-a \
+--machine-type=n1-standard-1 \
+--image=ubuntu-minimal-2004-focal-v20220622 \
+--image-project=ubuntu-os-cloud \
+--boot-disk-size=10GB \
+--boot-disk-type=pd-ssd \
+--boot-disk-device-name=dev-vm \
+--metadata-from-file=startup-script=dev.sh \
+--network-tier=STANDARD \
+--tags=http-server-8080
