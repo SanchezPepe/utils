a4781bdbb6aa354f73854bcbb0acd4a9163d5822
Pepe Sánchez <SanchezPepe@users.noreply.github.com>
2022-06-29 19:55:04 -0500

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -10,6 +10,18 @@ sudo apt --fix-broken install
 # Code Server Install
 curl -fsSL https://code-server.dev/install.sh | sh
 
+# To have systemd start code-server now and restart on boot:
+sudo systemctl enable --now code-server@$USER
+
+# Setting config file to access code-server on port
+echo
+{
+  echo 'bind-addr: 0.0.0.0:8080'
+  echo 'auth: password'
+  echo 'password: abc123'
+  echo 'cert: false'  
+} > /home/$USER/.config/code-server/config.yaml
+
 # Git config
 git config --global user.name "sanchezpepe"
 git config --global user.email sanchezpepe97@gmail.com
